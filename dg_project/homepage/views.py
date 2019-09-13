ad99c941c18bb5127bdaace71bd13f571d92760d
Bingzhong LI <bingzhong_li@outlook.com>
2019-09-13 16:14:28 +0800

    update homepage

--- dg_project/homepage/views.py
+++ dg_project/homepage/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Resort
-
+from django.views.decorators.cache import cache_page
+import random
+import os
 # Create your views here.
+# def randstring(num_string):
+#     randstr = "".join(random.sample(['z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a'], num_string))
+#     os.rename("".join([os.getcwd(),"\dg_project\homepage\static\index.css"]),"".join([os.getcwd(), f"\dg_project\homepage\static\index?version={randstr}.css"] ))
+#     return randstr
 
+# randstring(5)
 
-def home(request):
+@cache_page(10)
+def home(request): 
     context = {
+
         "resorts": Resort.objects.all()
     }
     return render(request, "home.html", context)
